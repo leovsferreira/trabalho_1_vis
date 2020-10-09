d052639e6ca9eb9e729f6c2e61f1939b80bad792
leonardo ferreira <leovsferreira@gmail.com>
2020-10-09 19:53:23 -0300

    adding examples to index js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ parametros de config
   2 - dimensoes da margem
   3 - escolher os labels de x e y
 */
+/*
+SCATTER PLOT EX
 let object = {div: '#main',
               width: 900,
               height: 600,
@@ -31,3 +33,47 @@ let object = {div: '#main',
             };
 
 startScatterChart(object);
+*/
+/*
+BAR CHART EX
+let object = {div: '#main',
+              width: 900,
+              height: 600,
+              top: 20,
+              left: 90,
+              bottom: 200,
+              right: 10,
+              xLabel: 'cities',
+              xLabelPos: [470, 550],
+              yLabel: 'deaths',
+              yLabelPos: [20, 225],
+              filePath: './data/corona_rj_data.csv',
+              rows: [49, 42, 79, 65, 56, 6], 
+              xData: 'city',
+              yData: 'deaths',
+              dataSelector: true};
+
+startBarChart(object);
+*/
+/*
+LINE CHART EX
+let object = {div: '#main',
+              width: 900,
+              height: 600,
+              top: 20,
+              left: 90,
+              bottom: 200,
+              right: 10,
+              xLabel: 'dates',
+              xLabelPos: [470, 500],
+              yLabel: 'death_rate',
+              yLabelPos: [20, 225],
+              filePath: './data/niteroi_series.csv',
+              rows: null, 
+              xData: 'date',
+              yData: 'death_rate',
+              lineWidth: 4,
+              dataSelector: true};
+
+startLineChart(object);
+*/
